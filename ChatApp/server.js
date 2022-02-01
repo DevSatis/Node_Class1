26d9c9287ed1c2ca9ced1720c98e815971342b62
Satish Kumar <sksatishk9@gmail.com>
2022-02-01 23:43:37 +0530

    Update Chat App

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -4,40 +4,60 @@ var app = express();
 var http = require('http').Server(app)
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
-
-
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://sksatishk9:Sumankusum@1@cluster0.2oups.mongodb.net/Chat_App?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+const { sendStatus } = require('express/lib/response');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
-// var dbUrl = "mongodb+srv://sksatishk9:Sumankusum@1@cluster0.2oups.mongodb.net/test"
-var messages=[
-    {name:'Satish',message:'hi'},
-    {name:'Vanshika',message:'hello'}
-]
+
+var dbUrl = "mongodb+srv://sksatishk9:Sumankusum@cluster0.2oups.mongodb.net/ChatAppData?retryWrites=true&w=majority";
+
+var Message = mongoose.model('Message',{
+    name: String,
+    message:String
+})
+
+
 app.get('/messages',(req, res)=>{
-    res.send(messages);
+    Message.find({},(err, messages)=>{
+        res.send(messages);
+    })
+    
 })
-app.post('/messages',(req, res)=>{
-    messages.push(req.body);
-    io.emit('message', req.body);
-    res.sendStatus(200);
+
+app.post('/messages',async (req, res)=>{
+
+    try {
+        
+        var message = new Message(req.body);
+
+        var savedMessage = await message.save()
+        
+        console.log("Saved")
+        var censored = await Message.findOne({message:'badword'})
+         if(censored){
+            await Message.remove({_id: censored.id})
+        }else 
+            io.emit('message', req.body);
+
+        res.sendStatus(200);
+
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error); 
+    }
+    
 })
+
+
 io.on('connection', (socket)=>{
     console.log("A user is connected")
 })
-mongoose.connect(uri,{useMongoClient: true}, (err)=>{
-    console.log("mongo DB Connection!", err)
+
+mongoose.connect(dbUrl, (err)=>{
+    console.log("Mongo DB Connection", err);
 })
+
 var server = http.listen(3000, () => {
     console.log('Server is listing on port!', server.address().port) 
 });
