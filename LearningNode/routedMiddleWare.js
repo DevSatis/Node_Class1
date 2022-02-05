ea0ba8666081a77fbee2244ddd52b656003a2db0
Satish Kumar <sksatishk9@gmail.com>
2022-02-05 23:01:23 +0530

    Express and Router

--- LearningNode/routedMiddleWare.js
+++ LearningNode/routedMiddleWare.js
@@ -5,13 +5,16 @@ const checkUrl= require('./rMiddleWare');
 
 // app.use(checkUrl)
 app.get('/',(req, res)=>{
-    res.send("This is Home Page! mware")
+    // res.send("This is Home Page! mware")
+    res.sendFile(__dirname+"/Home.html")
 });
 router.get('/about',checkUrl,(req, res)=>{
-    res.send("This is About Page! mware")
+    // res.send("This is About Page! mware")
+    res.sendFile(__dirname+"/About.html");
 });
 router.get('/login',checkUrl,(req, res)=>{
-    res.send("This is Login Page! mware")
+    // res.send("This is Login Page! mware");
+    res.sendFile(__dirname+"/Login.html")
 });
 app.use('/',router);
 app.listen(4500);
